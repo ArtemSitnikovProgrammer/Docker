814c51c5010964b59d5f8e3ddff6ec9d6fc2d8fc
Artem Sitnikov <timonsitnik@gmail.com>
2024-03-14 23:08:45 +0700

    Clear

--- sitnikovap.ru/html/wp-content/plugins/pagelayer/js/blocks/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ec115032f5549928ccbd');
